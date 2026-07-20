db364efdf543c13150e4197d2eac700482c937f5
dyllanrussell8-lang <dyllanrussell8@gmail.com>
2026-07-20 14:55:57 -0400

    Create alu.v

---
+++ alu.v
@@ -0,0 +1,19 @@
+module alu (
+    input  [7:0] a,
+    input  [7:0] b,
+    input  [2:0] opcode,
+    output reg [7:0] result,
+    output zero
+);
+    always @(*) begin
+        case (opcode)
+            3'b000: result = a + b;
+            3'b001: result = a - b;
+            3'b010: result = a & b;
+            3'b011: result = a | b;
+            3'b100: result = a << 1;
+            default: result = 8'b0;
+        endcase
+    end
+    assign zero = (result == 8'b0);
+endmodule
